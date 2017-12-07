a08f775c4228df9c98047d6e03082e7fb995e51c
Sasank Yadavalli <yadavallisasank@gmail.com>
2017-12-06 22:17:46 -0800

    Initial commit with basic scaffolding

---
+++ lib/ex_imgur.ex
@@ -0,0 +1,18 @@
+defmodule ExImgur do
+  @moduledoc """
+  Documentation for ExImgur.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExImgur.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_imgur_test.exs
@@ -0,0 +1,8 @@
+defmodule ExImgurTest do
+  use ExUnit.Case
+  doctest ExImgur
+
+  test "greets the world" do
+    assert ExImgur.hello() == :world
+  end
+end
